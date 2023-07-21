43c3b7920973cf3af81c58279dd3cfd9e3d0e84e
Jas Tandon <jtandon@sigmasolve.net>
2023-07-21 12:01:50 -0500

    na

--- domain.tf
+++ domain.tf
@@ -1,7 +1,7 @@
 resource "aws_cognito_user_pool_domain" "domain" {
   count = !local.enabled || module.context.domain_name == null || module.context.domain_name == "" ? 0 : 1
 
-  domain          = module.context.domain_name
+  domain          = join("", module.context.domain_name)
   certificate_arn = var.acm_certificate_arn
   user_pool_id    = try(aws_cognito_user_pool.pool[0].id, "")
 }
