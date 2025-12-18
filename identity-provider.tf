48bb174e4a6396f07206fb7073ecdc69c9971d51
Adarsh Hiwrale <adarsh.hiwrale@7pi.co>
2025-12-19 00:37:03 +0530

    fix cognito drift

--- identity-provider.tf
+++ identity-provider.tf
@@ -9,4 +9,13 @@ resource "aws_cognito_identity_provider" "identity_provider" {
   attribute_mapping = lookup(element(var.identity_providers, count.index), "attribute_mapping", {})
   idp_identifiers   = lookup(element(var.identity_providers, count.index), "idp_identifiers", [])
   provider_details  = lookup(element(var.identity_providers, count.index), "provider_details", {})
+
+   # Ignore AWS-managed SAML metadata fields that are auto-populated
+  lifecycle {
+    ignore_changes = [
+      provider_details["ActiveEncryptionCertificate"],
+      provider_details["SLORedirectBindingURI"],
+      provider_details["SSORedirectBindingURI"]
+    ]
+  }
 }
